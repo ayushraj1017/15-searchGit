ae94bb9cb4b1276eccde5a0ba360779dbcd036ee
Ayush Raj <ayushraj101@gmail.com>
2021-05-23 21:41:19 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia
+        maiores rerum officiis soluta reprehenderit quibusdam repudiandae totam
+        ut veniam praesentium. Nesciunt corrupti ea nihil molestias. Asperiores
+        maxime error, ex architecto quos cupiditate natus rerum in repellendus
+        alias dicta doloremque atque et nemo perspiciatis quaerat, inventore
+        recusandae hic! Laborum, optio adipisci.
+      </p>
+    </section>
+  );
+};
+
+export default About;
